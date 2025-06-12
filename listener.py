b12216da39ad9c90372ee2cf441b694f2d44556e
disheiX <renzosda@gmail.com>
2025-06-11 20:41:14 -0400

    Removed unnecesary stuff

--- listener.py
+++ listener.py
@@ -3,17 +3,6 @@ from get_data import *
 
 windows = get_windows()
 for i in range(len(windows)):
-    print(i, windows[i].window_text())
+    print(i, windows[i].window_text(), (windows[i].handle, windows[i].process_id()))
 
 print(get_ids(windows[6]))
-
-handle, pid = 394860, 4652
-wrapper = get_wrapper(handle, pid)
-run = True
-
-def main_listener():
-    prev_song = clean_song(wrapper)        
-    data = get_data(wrapper)
-    while clean_song(wrapper) == prev_song:
-        sleep(3)
-    
